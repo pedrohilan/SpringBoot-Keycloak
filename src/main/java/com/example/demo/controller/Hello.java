fd9df0bc3235fcf7bb673849ef4a7519d47b8583
Pedro Hilan <pedro.hilan99@gmail.com>
2023-01-20 16:15:28 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class Hello {
+
+    @GetMapping(value = "hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
